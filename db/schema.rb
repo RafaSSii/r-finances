f9e518f8d4ea4d2360927d0b7d3c2f6edf40a900
RafaSSii <rafarsk78@gmail.com>
2025-11-24 08:05:26 -0300

    Edit Card Function | Database Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_19_194745) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_24_120000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "cards", force: :cascade do |t|
     t.string "color"
     t.datetime "created_at", null: false
+    t.integer "position", default: 0, null: false
     t.string "title"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
