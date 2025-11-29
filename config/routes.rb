31b3487126df2b955142d07bb4ad6364a8222242
RafaSSii <rafarsk78@gmail.com>
2025-11-29 00:21:09 -0300

    Routes Changes

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,5 @@ Rails.application.routes.draw do
   resources :users, only: [ :new, :create ]
   resources :sessions, only: [ :new, :create, :destroy ]
   resources :dashboard, only: [ :index ]
-  resources :cards, only: [ :create, :edit, :update, :destroy ]
+  resources :cards, only: [ :index, :create, :edit, :update, :destroy ]
 end
